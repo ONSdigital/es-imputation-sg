e5bbfde87f4c27cd58fa5bf852212e4ca2f44d81
Gen Reynolds <genmreynolds@hotmail.co.uk>
2019-07-03 09:29:42 +0100

    Updated code for the wrangler

--- calculate_imputation_factors_wrangler.py
+++ calculate_imputation_factors_wrangler.py
@@ -5,23 +5,6 @@ import os
 import pandas as pd
 import boto3
 
-# Set up clients
-sqs = boto3.client('sqs')
-lambda_client = boto3.client('lambda')
-sns = boto3.client('sns')
-
-# Sqs
-queue_url = os.environ['queue_url']
-sqs_messageid_name = os.environ['sqs_messageid_name']
-
-# Sns
-arn = os.environ['arn']
-checkpoint = os.environ['checkpoint']
-
-current_period = os.environ['period']
-method_name = os.environ['method_name']
-questions = os.environ['questions']
-
 
 def _get_traceback(exception):
     """
@@ -47,22 +30,44 @@ def lambda_handler(event, context):
     :param context: lambda context
     :return: string
     """
+    # environment variables
+    sqs = boto3.client('sqs')
+    lambda_client = boto3.client('lambda')
+    checkpoint = os.environ['checkpoint']
+
+    queue_url = os.environ['queue_url']
+    sqs_messageid_name = os.environ['sqs_messageid_name']
+
+    current_period = os.environ['period']
+    method_name = os.environ['method_name']
+    questions = os.environ['questions']
+
     try:
+        # read in data from the sqs queue
         response = sqs.receive_message(QueueUrl=queue_url)
         message = response['Messages'][0]
         message_json = json.loads(message['Body'])
+
+        # reciept handler used to clear sqs queue
+        receipt_handle = message['ReceiptHandle']
+
         data = pd.DataFrame(message_json)
 
         # create df columns needed for method
         for question in questions.split(' '):
-            data['imputation_factor_' + question] = 0.0
+            data['imputation_factor_' + question] = 0
 
         data_json = data.to_json(orient='records')
 
+        # invoke the method to calculate the factors
         calculate_factors = lambda_client.invoke(FunctionName=method_name, Payload=data_json)
         json_response = calculate_factors.get('Payload').read().decode("UTF-8")
 
+        # send data to sqs queue
         send_sqs_message(queue_url, json.loads(json_response), sqs_messageid_name)
+
+        ### COMMENTED OUT FOR TESTING ###
+        # sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
 
     except Exception as exc:
 
@@ -86,6 +91,11 @@ def send_sns_message(imputation_run_type, anomalies):
     :param anomalies: list of anomalies collected from the method.
     :return: json string
     """
+
+    arn = os.environ['arn']
+    checkpoint = os.environ['checkpoint']
+    sns = boto3.client('sns')
+
     sns_message = {
         "success": True,
         "module": "Imputation Calculate Imputation Factors",
@@ -109,6 +119,8 @@ def send_sqs_message(queue_url, message, output_message_id):
     :param output_message_id: The label of the record in the SQS queue - Type: String
     :return: None
     """
+    sqs = boto3.client('sqs')
+
     sqs.send_message(QueueUrl=queue_url,
                      MessageBody=message,
                      MessageGroupId=output_message_id,
