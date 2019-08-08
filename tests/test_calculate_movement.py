bac0579d5cfda0097d05f5728704558d674cf839
Mike Candlin <michael.candlin@ons.gov.uk>
2019-08-08 14:40:00 +0100

    whoops....

--- tests/test_calculate_movement.py
+++ tests/test_calculate_movement.py
@@ -22,28 +22,16 @@ class TestStringMethods(unittest.TestCase):
             aws_access_key_id="fake_access_key",
             aws_secret_access_key="fake_secret_key",
         )
-        s3 = boto3.resource(
-            "s3",
-            region_name="eu-west-1",
-            aws_access_key_id="fake_access_key",
-            aws_secret_access_key="fake_secret_key",
-        )
 
         client.create_bucket(Bucket="MIKE")
         with open("tests/fixtures/test_data.json", "rb") as file:
-            calculate_movement_wrangler.save_to_s3("MIKE","123", file)
+            calculate_movement_wrangler.save_to_s3("MIKE", "123", file)
 
-        response = calculate_movement_wrangler.read_data_from_s3("MIKE","123")
+        response = calculate_movement_wrangler.read_data_from_s3("MIKE", "123")
         assert response
 
     def test_value_error_method(self):
         response = calculate_movement_method.lambda_handler("", None)
-        assert 'success' in response
-        assert response['success'] is False
-        assert response['error'].__contains__("""Error validating environment params""")
-
-    def test_value_error_wrangles(self):
-        response = calculate_movement_wrangler.lambda_handler("", None)
         assert 'success' in response
         assert response['success'] is False
         assert response['error'].__contains__("""Error validating environment params""")
