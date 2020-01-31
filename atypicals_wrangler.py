589de7ba1aaa34d28f52b119979be7494250aee6
Mike Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-01-31 14:09:51 +0000

    moved infile and incomingmessagegroup

--- atypicals_wrangler.py
+++ atypicals_wrangler.py
@@ -17,8 +17,6 @@ class InputSchema(Schema):
     """
     checkpoint = fields.Str(required=True)
     bucket_name = fields.Str(required=True)
-    in_file_name = fields.Str(required=True)
-    incoming_message_group = fields.Str(required=True)
     method_name = fields.Str(required=True)
     out_file_name = fields.Str(required=True)
     sns_topic_arn = fields.Str(required=True)
@@ -58,13 +56,13 @@ def lambda_handler(event, context):
 
         checkpoint = config["checkpoint"]
         bucket_name = config["bucket_name"]
-        in_file_name = config["in_file_name"]
-        incoming_message_group = config["incoming_message_group"]
         method_name = config["method_name"]
         out_file_name = config["out_file_name"]
         sns_topic_arn = config["sns_topic_arn"]
         sqs_message_group_id = config["sqs_message_group_id"]
 
+        in_file_name = event['RuntimeVariables']["in_file_name"]
+        incoming_message_group = event['RuntimeVariables']["incoming_message_group"]
         sqs_queue_url = event['RuntimeVariables']["queue_url"]
         questions_list = event['RuntimeVariables']['questions_list']
 
