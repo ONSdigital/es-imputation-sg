c683900519a8fd3e56ca3b05a609380bcd4bb5e8
KrisRogos <krisrogos@gmail.com>
2020-02-17 12:29:06 +0000

    Type again
    This should be all for Imputation

--- calculate_movement_method.py
+++ calculate_movement_method.py
@@ -39,8 +39,8 @@ def lambda_handler(event, context):
 
         df = pd.DataFrame(json.loads(json_data))
 
-        sorted_current = df[df[period_column] == int(current_period)]
-        sorted_previous = df[df[period_column] == int(previous_period)]
+        sorted_current = df[df[period_column].astype('str') == str(current_period)]
+        sorted_previous = df[df[period_column].astype('str') == str(previous_period)]
 
         for question in questions_list:
 
