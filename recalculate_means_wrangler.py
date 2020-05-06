2870318d2deb3e989a254ab1579bce41a41672dd
Piwington <david@fides.me.uk>
2020-05-06 16:41:00 +0100

    Recalc

--- recalculate_means_wrangler.py
+++ recalculate_means_wrangler.py
@@ -8,13 +8,22 @@ from marshmallow import Schema, fields
 
 
 class EnvironmentSchema(Schema):
-    """
-    Class to set up the environment variables schema.
-    """
     bucket_name = fields.Str(required=True)
     checkpoint = fields.Str(required=True)
     method_name = fields.Str(required=True)
     run_environment = fields.Str(required=True)
+
+
+class RuntimeSchema(Schema):
+    distinct_values = fields.List(required=True)
+    in_file_name = fields.Str(required=True)
+    incoming_message_group_id = fields.Str(required=True)
+    location = fields.Str(required=True)
+    out_file_name = fields.Str(required=True)
+    outgoing_message_group_id = fields.Str(required=True)
+    questions_list = fields.List(required=True)
+    sns_topic_arn = fields.Str(required=True)
+    queue_url = fields.Str(required=True)
 
 
 def lambda_handler(event, context):
